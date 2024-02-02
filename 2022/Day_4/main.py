606e3f1d7ee1ae7ec1cac2ebe3c02474cb547e43
Andrea Sgarbi <andrea.sgarbi@marchesini.com>
2024-02-02 16:13:57 +0100

    add es02 day4 2022

--- 2022/Day_4/main.py
+++ 2022/Day_4/main.py
@@ -15,13 +15,23 @@ def check_range(range1, range2) -> bool:
     else:
         return False
 
+def check_range_v2(range1, range2) -> bool:
+    if (range1[0] >= range2[0] and range1[0] <= range2[1]) or (range1[1] >= range2[0] and range1[1] <= range2[1]):
+        return True
+    elif check_range(range1,range2):
+        return True
+    else:
+        return False
+
+
 
 def main():
 
     with open('input.txt', 'r') as f:
         lines = f.readlines()
 
-    sum = 0
+    sum_part1 = 0
+    sum_part2 = 0
     for line in lines:
         elf1, elf2 = line.split(',')
         if elf2[-1] == '\n':
@@ -30,10 +40,14 @@ def main():
         elf2 = calc_range(elf2)
 
         if check_range(elf1, elf2):
-            sum += 1
+            sum_part1 += 1
+
+        if check_range_v2(elf1, elf2):
+            sum_part2 += 1
 
 
-    print(sum)
+    print(sum_part1)
+    print(sum_part2)
 
 if __name__ == "__main__":
     main()
