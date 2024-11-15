fee6fca931b4cdc3265abb7e78bd08c8c6da37e4
Andrea Sgarbi <andrea.sgarbi@marchesini.com>
2024-11-15 15:07:42 +0100

    Add Day6 2022 Solution.

--- 2022/Day_6/main.py
+++ 2022/Day_6/main.py
@@ -3,21 +3,35 @@
 '''
 
 def check_start_packet(packet):
-   tmp = []
-   tmp.append(packet[0])
+   tmp = ''
+   tmp = tmp + packet[0]
    find = False
    i = 1
-   while (find == False):
+   while ((find == False) and (i < 14)):
       if (tmp.find(packet[i]) == -1):
+         tmp = tmp + packet[i]
          i = i + 1
-         tmp.append(packet[i])
+      else: 
+         find = True
+   return i
+   
 
 def main():
 
-   with open('test1.txt', 'r') as f:
-      line = f.readlines()
+   with open('input.txt', 'r') as f:
+      line = f.read()
 
-   print(line)
+   pattern_match = False
+   i = 0
+   while ((pattern_match == False) and (line[i + 14] != '\n')):
+      res = check_start_packet(line[i : i + 14])
+      if (res == 14):
+         pattern_match = True
+         res_match = i + 14
+      else:
+         i = i + 1
+      
+   print(res_match)
 
 
 if __name__ == "__main__":
