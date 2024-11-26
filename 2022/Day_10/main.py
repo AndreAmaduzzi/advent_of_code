2dfc08fefedf575f92d2da964025497277fc22d9
Andrea-Sgarbi <122173749+Andrea-Sgarbi@users.noreply.github.com>
2024-11-26 23:06:21 +0100

    Add day10 2022 part1 solution.

---
+++ 2022/Day_10/main.py
@@ -0,0 +1,41 @@
+'''
+9st December, 2022
+'''
+
+def main():
+    with open("input.txt") as f:
+        rows = f.readlines()
+
+    cycle = 0
+    X = 1
+    cycle_sampling = 20
+    signal_strength = 0
+    for line in rows:
+        cmd = line.split()[0]
+        if (cmd == "addx"):
+            value = (int) (line.split()[1])
+        
+        if (cmd == "noop"):
+            cycle += 1
+            if (cycle >= cycle_sampling):
+                signal_strength += cycle_sampling * X
+                cycle_sampling += 40
+        elif (cmd == "addx"):
+            value = int(value)
+            if (cycle + 1 >= cycle_sampling):
+                signal_strength += cycle_sampling * X
+                cycle_sampling += 40
+            cycle += 2
+            if (cycle >= cycle_sampling):
+                signal_strength += cycle_sampling * X
+                cycle_sampling += 40
+            X += value
+
+        if (cycle >= 220):
+            break
+
+    print(signal_strength)
+    
+
+if __name__ == "__main__":
+    main()
