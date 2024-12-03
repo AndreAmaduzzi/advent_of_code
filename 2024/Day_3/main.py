3a746842321dbc257faff32d8c175cfbdc38d96e
Andrea Sgarbi <andrea.sgarbi@marchesini.com>
2024-12-03 14:00:47 +0100

    Add Day3 2024 files.

---
+++ 2024/Day_3/main.py
@@ -0,0 +1,39 @@
+"""
+3st December, 2024
+"""
+
+
+import re
+
+
+def clean(s):
+    p = re.compile('mul\([0-9]{1,3},[0-9]{1,3}\)')
+    result = p.findall(s)
+    return result
+
+def mul(s):
+    l = list(map(int,s.strip('mul()').split(',')))
+    return (l[0] * l[1])
+
+
+def main():
+   with open('test.txt') as f:
+         data = f.read()
+
+   partition = re.split('(do\(\)|don\'t\(\))', data)
+
+   total = 0
+   do = True
+   for s in partition:
+      if s == 'do()':
+         do = True
+      elif s == "don't()":
+         do = False
+      if (do):
+         for x in clean(s):
+               total = total + mul(x)
+
+   print(total)
+
+if __name__ == "__main__":
+    main()
