8f238158a87f34b5ce23974c50f26302d4d2efcf
Andrea-Sgarbi <122173749+Andrea-Sgarbi@users.noreply.github.com>
2025-03-02 22:22:54 +0100

    Add Day19 2024 part1 Solution.

--- 2024/Day_19/main.py
+++ 2024/Day_19/main.py
@@ -4,14 +4,24 @@
 
 
 def main():
-   source = "test.txt"
+   source = "input.txt"
    with open(source) as f:
       lines = f.read()
 
-   patterns, messages = lines.split("\n\n")
+   patterns, desired = lines.split("\n\n")
 
-   print(patterns)
-   print(messages)
+   patterns = patterns.split(", ")
+   desired = desired.split("\n")
+
+   def count_matches(i, design):
+      if i == len(design): return True
+      for towel in patterns:
+         if i + len(towel) <= len(design) and design[i:i + len(towel)] == towel and count_matches(i + len(towel), design): return True
+      return False
+
+   match_count = sum([1 for design in desired if count_matches(0, design)])
+   print(f"Number of matches: {match_count}")
+
 
 if __name__ == "__main__":
     main()
