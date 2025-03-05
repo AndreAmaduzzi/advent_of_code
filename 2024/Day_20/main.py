f2ef1e545f4ec1dd9087c25bbf75a49b0b1de7cc
Andrea-Sgarbi <122173749+Andrea-Sgarbi@users.noreply.github.com>
2025-03-05 16:15:50 +0100

    Add Day20 2024 part1 solution.

--- 2024/Day_20/main.py
+++ 2024/Day_20/main.py
@@ -35,31 +35,64 @@ def get_direction(map, x, y, old_dir=None):
 
 def path_iter(map, x, y):
    score = {}
+   score[(x, y)] = 0
+   path = []
    count = 0
    old_dir = None
    while map[y][x] != "E":
       dir = get_direction(map, x, y, old_dir)
+      path.append(dir)
       dx, dy = direction[dir]
       x += dx
       y += dy
       count += 1
       score[(x, y)] = count
       old_dir = dir
-   return score
+   return score, path
+
+
+def calc_cheats(map, score, path, x, y, target):
+   cheats = 0
+   for i, dir in enumerate(path):
+
+      if map[y-1][x] == "#":
+         if (x, y-2) in score:
+            if score[(x, y-2)] - score[(x, y)] - 2 >= target:
+               cheats += 1
+      if map[y+1][x] == "#":
+         if (x, y+2) in score:
+            if score[(x, y+2)] - score[(x, y)] - 2 >= target:
+               cheats += 1
+      if map[y][x-1] == "#":
+         if (x-2, y) in score:
+            if score[(x-2, y)] - score[(x, y)] - 2 >= target:
+               cheats += 1
+      if map[y][x+1] == "#":
+         if (x+2, y) in score:
+            if score[(x+2, y)] - score[(x, y)] - 2 >= target:
+               cheats += 1
+      dx, dy = direction[dir]
+      x += dx
+      y += dy
+   return cheats
 
 
 def main():
-   source = "test.txt"
+   source = "input.txt"
    with open(source) as f:
       lines = f.readlines()
 
    map = [list(row[:-1]) for row in lines]
 
    x, y = init_path(map)
+   x_start = x
+   y_start = y
 
-   score = path_iter(map, x, y)
+   score, path = path_iter(map, x, y)
 
-   print(score)
+   res = calc_cheats(map, score, path, x_start, y_start, 100)
+
+   print(res)
 
 if __name__ == "__main__":
     main()
