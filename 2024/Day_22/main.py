e03fe0f6c232e84357494779b900e09fd3e5edb1
Andrea-Sgarbi <122173749+Andrea-Sgarbi@users.noreply.github.com>
2025-03-09 15:45:55 +0100

    Add Day22 2024 part1 solution.

--- 2024/Day_22/main.py
+++ 2024/Day_22/main.py
@@ -3,14 +3,28 @@
 """
 
 
+def calc_secret(num, iterations):
+   prune_mask = (1 << 24) - 1
+   for _ in range(iterations):
+      num ^= (num << 6) & prune_mask
+      num ^= (num >> 5) 
+      num ^= (num << 11) & prune_mask
+   return num
+
+
 def main():
-   source = "test.txt"
+   source = "input.txt"
    with open(source) as f:
       lines = f.readlines()
 
    secrets = [line.strip() for line in lines]
 
-   print(secrets)
+   sum = 0
+   for i in range(len(secrets)):
+      sum += calc_secret(int(secrets[i]), 2000)
+   
+   print(sum)
+
 
 if __name__ == "__main__":
     main()
