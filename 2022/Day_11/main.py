d6b724e599acfd34da086b712599902c004beef5
Andrea-Sgarbi <122173749+Andrea-Sgarbi@users.noreply.github.com>
2025-07-11 22:12:01 +0200

    Add Day 11 2022 Solution.

---
+++ 2022/Day_11/main.py
@@ -0,0 +1,55 @@
+'''
+11st December, 2022
+'''
+
+
+def main():
+   with open("input.txt") as f:
+      ls = f.read().split("\n\n")
+
+   monkeys = {}
+   for element in ls:
+      line = element.split("\n")
+      curr_monkey = {}
+      for word in line:
+         word = word.strip()
+         if word.startswith("Monkey"):
+            monkey_id = int(word.split(" ")[1][:-1])
+         elif word.startswith("Starting items:"):
+            curr_monkey["items"] = [int(x) for x in word.split(":")[1].split(",")]
+         elif word.startswith("Operation:"):
+            curr_monkey["operation"] = word.split(":")[1].strip()
+         elif word.startswith("Test:"):
+            curr_monkey["test"] = int(word.split(" ")[3].strip())
+         elif word.startswith("If true:"):
+            curr_monkey["true_target"] = int(word.split(" ")[5].strip())
+         elif word.startswith("If false:"):
+            curr_monkey["false_target"] = int(word.split(" ")[5].strip())
+      curr_monkey["inspections"] = 0
+      monkeys[monkey_id] = curr_monkey
+   
+   for i in range(20):
+      for monkey_id, monkey in monkeys.items():
+         items = monkey["items"]
+         monkey["items"] = []
+         for item in items:
+            old_value = item
+            operation = monkey["operation"].replace("old", str(old_value))
+            if '=' in operation:
+               expression = operation.split('=')[1].strip()
+            else:
+               expression = operation.strip()
+            new_value = eval(expression) // 3
+            if new_value % monkey["test"] == 0:
+               target_monkey = monkey["true_target"]
+            else:
+               target_monkey = monkey["false_target"]
+            monkeys[target_monkey]["items"].append(new_value)
+            monkey["inspections"] += 1
+
+   max_inspections = sorted([monkey["inspections"] for monkey in monkeys.values()], reverse=True)
+   result = max_inspections[0] * max_inspections[1]
+   print(f"Result: {result}")
+
+if __name__ == "__main__":
+    main()
